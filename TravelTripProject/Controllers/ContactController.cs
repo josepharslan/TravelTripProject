ecb6b731c19ce907a21015539edc4bcfcbc5d579
josepharslan <markreddcarter@gmail.com>
2025-04-16 22:45:35 +0300

    Add project files.

---
+++ TravelTripProject/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProject.Models.Siniflar;
+
+namespace TravelTripProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        Context c = new Context();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Iletisim ilt)
+        {
+            c.Iletisims.Add(ilt);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
